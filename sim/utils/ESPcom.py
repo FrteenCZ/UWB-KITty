a8187daedd24e35d93cbe1756cc0e7afd31fb6d8
FrteenCZ <franta11franta@gmail.com>
2025-07-04 22:46:10 +0200

    enhance parsing json from ESP

--- sim/utils/ESPcom.py
+++ sim/utils/ESPcom.py
@@ -21,12 +21,16 @@ class SerialThread(threading.Thread):
         self.keep_running = True
         self.latest_line = ""
 
+        self.trilateration_cords = None
+        self.kalman_cords = None
+
     def run(self):
         while self.keep_running:
             if self.ser.in_waiting:
                 line = self.ser.readline().decode().strip()
                 print(f"[ESP] {line}")
                 self.latest_line = line
+                self.process_latest_line()
             else:
                 time.sleep(0.1)
 
@@ -66,3 +70,23 @@ class SerialThread(threading.Thread):
                 data = json.dumps(points)
                 self.send_command(f"points {data}", silent=True)
                 print(f"[BLENDER] Sent points")
+
+    def process_latest_line(self):
+        if self.latest_line.startswith("Trilateration Solution:"):
+            try:
+                data = self.latest_line.split(":")[1].strip()
+                self.trilateration_cords = json.loads(data)
+                [x, y, z] = self.trilateration_cords[0]
+                print(f"[BLENDER] Trilateration coordinates: x={x}, y={y}, z={z}")
+            except json.JSONDecodeError as e:
+                print(f"[BLENDER] Error decoding trilateration data: {e}")
+
+        elif self.latest_line.startswith("Kalman Filter State JSON:"):
+            try:
+                data = self.latest_line.split(":")[1].strip()
+                self.kalman_cords = json.loads(data)
+                [x, y, z, vx, vy, vz] = self.kalman_cords[0]
+                print(f"[BLENDER] Kalman coordinates: x={x}, y={y}, z={z}, vx={vx}, vy={vy}, vz={vz}")
+            except json.JSONDecodeError as e:
+                print(f"[BLENDER] Error decoding Kalman data: {e}")
+
