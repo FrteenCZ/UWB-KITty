c840b96ef45c65f24d3090f03639709ea6ed24fe
FrteenCZ <franta11franta@gmail.com>
2025-09-07 22:25:43 +0200

    feat: enhance scene update to visualize solution space in undetermined cases

--- sim/operators/comunication.py
+++ sim/operators/comunication.py
@@ -1,8 +1,18 @@
 import bpy  # type: ignore
+import numpy as np
+import math
 from ..utils.ESPcom import SerialThread
 
 
-def ensure_arrow(name, start=(0, 0, 0), end=(0, 0, 0), relative=False):
+def cleanup_collection(coll, keep_names=None):
+    keep_names = set(keep_names or [])
+
+    for obj in list(coll.objects):
+        if obj.name not in keep_names:
+            bpy.data.objects.remove(obj, do_unlink=True)
+
+
+def ensure_arrow(name, start=(0, 0, 0), end=(0, 0, 0), relative=False, coll=None):
     """Create or update an arrow (mesh line) between start and end."""
     if relative:
         end = (start[0] + end[0], start[1] + end[1], start[2] + end[2])
@@ -13,6 +23,8 @@ def ensure_arrow(name, start=(0, 0, 0), end=(0, 0, 0), relative=False):
         mesh = bpy.data.meshes.new(name + "Mesh")
         obj = bpy.data.objects.new(name, mesh)
         bpy.context.collection.objects.link(obj)
+        if coll is not None:
+            coll.objects.link(obj)
 
     # Update geometry
     mesh = obj.data
@@ -27,20 +39,109 @@ def update_scene(thread):
     if thread.trilateration_cords is not None and thread.alpha is not None and thread.null_space is not None:
         [x, y, z] = thread.trilateration_cords[0]
         alpha = thread.alpha
+        null_space = np.array(thread.null_space)
 
-        for i, vec in enumerate(thread.null_space):
-            ensure_arrow(f"NullSpace{i+1}", (x, y, z),
-                         vec, relative=True)  # * alpha
+        coll = bpy.data.collections.get("Trilateration")
+        if not coll:
+            coll = bpy.data.collections.new("Trilateration")
+            bpy.context.scene.collection.children.link(coll)
 
-        obj = bpy.data.objects.get("Trilateration")
-        if obj is None:
-            bpy.ops.object.empty_add(type='CUBE')
-            obj = bpy.context.active_object
-            obj.name = "Trilateration"
-            obj.scale = (1.2, 1.2, 1.2)
+        # 3D subspace
+        if len(null_space) == 0:
+            cleanup_collection(coll, ["Solution"])
+            obj = bpy.data.objects.get("Solution")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CUBE')
+                obj = bpy.context.active_object
+                obj.name = "Solution"
+                obj.scale = (1.2, 1.2, 1.2)
+                coll.objects.link(obj)
 
-        obj.location = (x, y, z)
+            obj.location = (x, y, z)
 
+        # 2D subspace
+        elif len(null_space) == 1:
+            cleanup_collection(
+                coll, ["Center", "Normal", "Solution", "AntiNormal", "AntiSolution"])
+            obj = bpy.data.objects.get("Center")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CUBE')
+                obj = bpy.context.active_object
+                obj.name = "Center"
+                obj.scale = (0.1, 0.1, 0.1)
+                coll.objects.link(obj)
+            obj.location = (x, y, z)
+
+            ensure_arrow("Normal", (x, y, z),
+                         null_space[0] * alpha, relative=True, coll=coll)
+            obj = bpy.data.objects.get("Solution")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CUBE')
+                obj = bpy.context.active_object
+                obj.name = "Solution"
+                obj.scale = (1.2, 1.2, 1.2)
+                coll.objects.link(obj)
+            obj.location = (x, y, z) + alpha * null_space[0]
+
+            ensure_arrow("AntiNormal", (x, y, z), -
+                         null_space[0] * alpha, relative=True, coll=coll)
+            obj = bpy.data.objects.get("AntiSolution")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CUBE')
+                obj = bpy.context.active_object
+                obj.name = "AntiSolution"
+                obj.scale = (1.2, 1.2, 1.2)
+                coll.objects.link(obj)
+            obj.location = (x, y, z) - alpha * null_space[0]
+
+        # 1D subspace
+        elif len(null_space) == 2:
+            cleanup_collection(coll, ["Center", "1DSolutionSpace"])
+            obj = bpy.data.objects.get("Center")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CUBE')
+                obj = bpy.context.active_object
+                obj.name = "Center"
+                obj.scale = (0.1, 0.1, 0.1)
+                coll.objects.link(obj)
+            obj.location = (x, y, z)
+
+            obj = bpy.data.objects.get("1DSolutionSpace")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CIRCLE')
+                obj = bpy.context.active_object
+                obj.name = "1DSolutionSpace"
+                coll.objects.link(obj)
+            obj.location = (x, y, z)
+            obj.scale = (alpha, alpha, alpha)
+            normalVec = np.cross(null_space[0], null_space[1])
+            r = np.sqrt(normalVec[0]**2 + normalVec[1]**2 + normalVec[2]**2)
+            theta = np.arccos(normalVec[2] / r) if r != 0 else 0.0
+            phi = np.arctan2(normalVec[1], normalVec[0])
+            obj.rotation_euler = (np.pi/2 - theta, 0, phi - np.pi/2)
+
+
+        # 0D subspace (BROKEN ON ESP!)
+        elif len(null_space) == 3:
+            cleanup_collection(coll, ["Center", "0DSolutionSpace"])
+            obj = bpy.data.objects.get("Center")
+            if obj is None:
+                bpy.ops.object.empty_add(type='CUBE')
+                obj = bpy.context.active_object
+                obj.name = "Center"
+                obj.scale = (0.1, 0.1, 0.1)
+                coll.objects.link(obj)
+            obj.location = (x, y, z)
+
+            obj = bpy.data.objects.get("0DSolutionSpace")
+            if obj is None:
+                bpy.ops.mesh.primitive_uv_sphere_add()
+                obj = bpy.context.active_object
+                obj.name = "0DSolutionSpace"
+                coll.objects.link(obj)
+            obj.location = (x, y, z)
+            obj.scale = (alpha, alpha, alpha)
+            
     # Kalman Filter State
     if thread.kalman_cords is not None:
         [x, y, z, vx, vy, vz] = thread.kalman_cords[0]
