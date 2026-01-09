aa5b33dbcbc2391f40ad9d14bda93a4e9697f236
FrteenCZ <franta11franta@gmail.com>
2026-01-09 08:57:46 +0100

    Update platformio.ini and config.h for new board configuration

--- src/src/config.h
+++ src/src/config.h
@@ -1,16 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const int onboardledPin = 2;
-
+#define BUTTON_PIN  0   // User Button
+#define LED_PIN     1   // Status LED
+#define BOOT_PIN    9   // Boot Mode Pin
 
 // Define the pin configuration for DWM1000
-#define UWB_PIN_SPI_MISO  12    // SPI MISO
-#define UWB_PIN_SPI_MOSI  13    // SPI MOSI
-#define UWB_PIN_SPI_SCK   14    // SPI Clock
-#define UWB_PIN_SPI_SS    15    // Chip Select
-#define UWB_PIN_SPI_RST   16    // Reset
-#define UWB_PIN_SPI_IRQ   17    // Interrupt
+#define UWB_PIN_SPI_IRQ     3   // Interrupt
+#define UWB_PIN_SPI_SCK     4   // SPI Clock
+#define UWB_PIN_SPI_MISO    5   // SPI MISO
+#define UWB_PIN_SPI_MOSI    6   // SPI MOSI
+#define UWB_PIN_SPI_SS      7   // Chip Select
+#define UWB_PIN_SPI_RST     10  // Reset
 
 // Define the default antenna delay
 #define DEFAULT_ANTENNA_DELAY 16150 // Default antenna delay in picoseconds
